16fc13955b86d71e322312c3a33ad8626659a96d
Alexander Angove Lilja <nalleballeuwu@gmail.com>
2024-06-01 18:33:44 +0200

    Add project files.

---
+++ dockerLab/Data/BookDbContext.cs
@@ -0,0 +1,14 @@
+﻿using dockerLab.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dockerLab.Data
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
